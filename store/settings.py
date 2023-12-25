b606c0e97124c65f8e1c7488d1c9887a0ac127c7
dima9209 <152408060+dima9209@users.noreply.github.com>
2023-12-25 19:14:44 +0300

    Создание нового приложения users и создание новой модели БД на основании класса AbstractUsers

--- store/settings.py
+++ store/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products'
+    'products',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#User
+
+AUTH_USER_MODEL = 'users.User'
