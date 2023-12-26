265db576e295a5ced001b043704f44e64ec3ed3f
dima9209 <152408060+dima9209@users.noreply.github.com>
2023-12-26 20:25:41 +0300

    Добавление формы авторизации пользователя

--- users/views.py
+++ users/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
+from users.models import User
+from users.forms import UserLoginForm
 
 
 def login(request):
     content = {
-        'title': 'Store - Авторизация'
+        'title': 'Store - Авторизация',
+        'form': UserLoginForm()
     }
     return render(request, 'users/login.html', content)
 
 
-def register(request):
+def registration(request):
     content = {
         'title': 'Store - Регистрация'
     }
