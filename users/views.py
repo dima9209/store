2266f7b4f5df2b3c06fc20454145130169ee2e47
dima9209 <152408060+dima9209@users.noreply.github.com>
2023-12-26 21:14:57 +0300

    Добавление в контролер с логированием валидации, аунтидификации и авторизации

--- users/views.py
+++ users/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from users.models import User
 from users.forms import UserLoginForm
+from django.contrib import auth
+from django.urls import reverse
 
 
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+
     content = {
         'title': 'Store - Авторизация',
-        'form': UserLoginForm()
+        'form': form
     }
     return render(request, 'users/login.html', content)
 
