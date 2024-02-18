0c42c182f78036808b8d3c4f5fb7557fe460f1f6
dima9209 <152408060+dima9209@users.noreply.github.com>
2024-02-18 11:01:00 +0300

    bugfix заказов

--- orders/urls.py
+++ orders/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
-from orders.views import (CancelTemplateView, CreateOrderView, DetailView,
+from orders.views import (CancelTemplateView, CreateOrderView, OrderDetailView,
                           OrderListView, SuccessTemplateView)
 
 app_name = 'orders'
 
 urlpatterns = [
     path('', OrderListView.as_view(), name='orders'),
-    path('detail/<int:pk>/', DetailView.as_view(), name='orders_details'),
+    path('detail/<int:pk>/', OrderDetailView.as_view(), name='orders_details'),
     path('create_order/', CreateOrderView.as_view(), name='create_order'),
     path('order-success/', SuccessTemplateView.as_view(), name='order_success'),
     path('order-cancel/', CancelTemplateView.as_view(), name='order_canceling'),
